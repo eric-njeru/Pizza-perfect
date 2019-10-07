99b0c793900aed084ef6904f976aba5fe6f8b3d3
eric-njeru <nericokush@gmail.com>
2019-10-07 05:52:44 +0300

    Add sauce

--- js/receiptscript.js
+++ js/receiptscript.js
@@ -44,4 +44,102 @@ function getCrust(runningTotal,orderText,subtotalText) {
     }
     runningTotal = (runningTotal + crustTotal);
     subtotalText = subtotalText + crustTotal + "<br>";
-  
+  getSauce(runningTotal,orderText,subtotalText);
+};
+
+function getSauce(runningTotal,orderText,subtotalText) {
+    var sauceArray = document.getElementsByName("sauce");
+    for (var j = 0; j < sauceArray.length; j++) {
+        if (sauceArray[j].checked) {
+            selectedSauce = sauceArray[j].value;
+            orderText = orderText + selectedSauce +"<br>";
+        }
+    }
+    subtotalText = subtotalText + 0 + "<br>";
+    getMeat(runningTotal,orderText,subtotalText);
+};
+
+function getMeat(runningTotal,orderText,subtotalText) {
+    var runningTotal = runningTotal;
+    var meatTotal = 0;
+    var selectedMeat = [];
+    var meatArray = document.getElementsByName("meat");
+    for (var j = 0; j < meatArray.length; j++) {
+        if (meatArray[j].checked) {
+            selectedMeat.push(meatArray[j].value);
+        }
+    }
+    var meatCount = selectedMeat.length;
+    if (meatCount > 1) {
+        meatTotal = (meatCount - 1);
+    } else {
+        meatTotal = 0;
+    }
+    runningTotal = (runningTotal + meatTotal);
+    for (var j = 0; j < selectedMeat.length; j++) {
+            orderText = orderText+selectedMeat[j]+"<br>";
+            if (meatCount <= 1) {
+                subtotalText = subtotalText + 0 + "<br>";
+                meatCount = meatCount - 1;
+            } else if (meatCount == 2) {
+                subtotalText = subtotalText + 1 + "<br>";
+                meatCount = meatCount - 1;
+            } else {
+                subtotalText = subtotalText + 1 + "<br>";
+                meatCount = meatCount - 1;
+            }
+    }
+    getVeggie(runningTotal,orderText,subtotalText);
+};
+
+function getVeggie(runningTotal,orderText,subtotalText) {
+    var veggieTotal = 0;
+    var selectedVeggie = [];
+    var veggieArray = document.getElementsByName("veggies");
+    for (var j = 0; j < veggieArray.length; j++) {
+        if (veggieArray[j].checked) {
+            selectedVeggie.push(veggieArray[j].value);
+        }
+    }
+    var veggieCount = selectedVeggie.length;
+    if (veggieCount > 1) {
+        veggieTotal = (veggieCount - 1);
+    } else {
+        veggieTotal = 0;
+    }
+    runningTotal = (runningTotal + veggieTotal);
+    for (var j = 0; j < selectedVeggie.length; j++) {
+            orderText = orderText+selectedVeggie[j]+"<br>";
+            if (veggieCount <= 1) {
+                subtotalText = subtotalText + 0 + "<br>";
+                veggieCount = veggieCount - 1;
+            } else if (veggieCount == 2) {
+                subtotalText = subtotalText + 1 + "<br>";
+                veggieCount = veggieCount - 1;
+            } else {
+                subtotalText = subtotalText + 1 + "<br>";
+                veggieCount = veggieCount - 1;
+            }
+    }
+    getCheese(runningTotal,orderText,subtotalText);
+};
+
+function getCheese(runningTotal,orderText,subtotalText) {
+    var cheeseTotal = 0;
+    var selectedCheese = [];
+    var cheeseArray = document.getElementsByName("cheese");
+    for (var j = 0; j < cheeseArray.length; j++) {
+        if (cheeseArray[j].checked) {
+            selectedCheese = cheeseArray[j].value;
+        }
+        if (selectedCheese === "extra") {
+            cheeseTotal = 3;
+        }
+    }
+    runningTotal = (runningTotal + cheeseTotal);
+    subtotalText = subtotalText+cheeseTotal+"<br>";
+    orderText = orderText+selectedCheese+"<br>";
+    document.getElementById("showOrderText").innerHTML=orderText;
+    document.getElementById("showSubtotalText").innerHTML=subtotalText;
+    document.getElementById("totalPrice").innerHTML = "</h3>$"+runningTotal+".00"+"</h3>";
+};
